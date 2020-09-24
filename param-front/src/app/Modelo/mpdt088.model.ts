530208f9d0ff150bfbdf7deb85c2509c163aec20
ElenaOroSil <elena@oneproject.com>
2020-09-23 20:26:25 -0500

    model mpdt088

---
+++ param-front/src/app/Modelo/mpdt088.model.ts
@@ -0,0 +1,32 @@
+
+export class Mpdt088 {
+
+    codent: string;
+    producto: string;
+    subprodu: string;
+    codmar: number;
+    indtipt: number;
+    fecalta: string;
+    fecbaja: string;
+    limcremes: number;
+    limdebmes: number;
+    maxoper: number;
+    limcredia: number;
+    limdebdia: number;
+    limdiacaj: number;
+    maxopecaj: number;
+    maxodia: number;
+    salmaxmon: number;
+    cargamax: number;
+    consumax: number;
+    indacpalt: string;
+    indacpren: string;
+    vigtar: number;
+    vigtarren: number;
+    codentumo: string;
+    codofiumo: string;
+    usuarioumo: string;
+    codtermumo: string;
+    contcur: string;
+
+}
