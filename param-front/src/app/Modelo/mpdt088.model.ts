c7cdcb6e994f1201f327ab5a1e9dbc6e43a5662a
ElenaOroSil <elena@oneproject.com>
2020-09-23 21:20:33 -0500

    mpdt088

--- param-front/src/app/Modelo/mpdt088.model.ts
+++ param-front/src/app/Modelo/mpdt088.model.ts
@@ -29,4 +29,36 @@ export class Mpdt088 {
     codtermumo: string;
     contcur: string;
 
+    constructor (mpdt088) {
+
+    this.codent = mpdt088.codent;
+    this.producto = mpdt088.producto;
+    this.subprodu = mpdt088.subprodu;
+    this.codmar = mpdt088.codmar;
+    this.indtipt = mpdt088.indtipt;
+    this.fecalta = mpdt088.fecalta;
+    this.fecbaja = mpdt088.fecbaja;
+    this.limcremes = mpdt088.limcremes;
+    this.limdebmes = mpdt088.limdebmes;
+    this.maxoper = mpdt088.maxoper;
+    this.limcredia = mpdt088.limcredia;
+    this.limdebdia = mpdt088.limdebdia;
+    this.limdiacaj = mpdt088.limdiacaj;
+    this.maxopecaj = mpdt088.maxopecaj;
+    this.maxodia = mpdt088.maxodia;
+    this.salmaxmon = mpdt088.salmaxmon;
+    this.cargamax = mpdt088.cargamax;
+    this.consumax = mpdt088.consumax;
+    this.indacpalt = mpdt088.indacpalt;
+    this.indacpren = mpdt088.indacpren;
+    this.vigtar = mpdt088.vigtar;
+    this.vigtarren = mpdt088.vigtarren;
+    this.codentumo = mpdt088.codentumo;
+    this.codofiumo = mpdt088.codofiumo;
+    this.usuarioumo = mpdt088.usuarioumo;
+    this.codtermumo = mpdt088.codtermumo;
+    this.contcur = mpdt088.contcur;
+
+    }
+
 }
